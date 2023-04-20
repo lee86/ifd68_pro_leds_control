b19b26eaf6672642ca845c571cf0b8c38ccab0e9
jiangwe leo <1099986768@qq.com>
2023-04-20 20:23:07 +0800

    初步测试音律的灯光控制

--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
 
 var productIDGD = []byte{0x002d, 0x002c}
 
 const (
-	vendorID  uint16 = 0x3318 // 替换为你的设备的_VENDOR_ID
-	productID uint16 = 0x0424 // 替换为你的设备的_PRODUCT_ID
-
 	vendorIDGD uint16 = 0x31d6
 )
 
 var testIs = flag.Bool("t", false, "测试开关")
 
 func main() {
-	if flag.Parsed() {
-		ifd := new(Ifd68Pro)
-		ifd.init()
-		ifd.hidapi()
-		go ifd.startServer()
+	fmt.Println("ifd68 pro control")
+	if !flag.Parsed() {
+		flag.Parse()
 	}
+
+	fmt.Println("参数加载完成")
+	ifd := new(Ifd68Pro)
+	ifd.init()
+	go ifd.hidapi()
+	go ifd.startServer()
+
+	fmt.Println("over boy!")
 	select {}
 }
